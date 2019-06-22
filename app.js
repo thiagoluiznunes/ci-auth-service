933ae839690174557585167372de2ce8f0801c76
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-06-22 18:55:46 -0300

    create db connection to maLab db

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-db.initConnection('ci-auth-db');
+db.initConnection();
 
 app.use('/api/v1', mock.api);
 app.use('/api/v1/auth', authRoutes);
--- config/db.js
+++ config/db.js
@@ -1,7 +1,36 @@
 import mongoose from 'mongoose';
+import { NODE_ENV, DB_NAME, DB_USER, DB_USER_PASS } from 'babel-dotenv';
 
-const initConnection = (dbName) => {
-  mongoose.connect(`mongodb://localhost:27017/${dbName}`, { useNewUrlParser: true });
+const initConnection = async () => {
+  if (NODE_ENV === 'dev') {
+    mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`, { useNewUrlParser: true });
+  } else {
+    const uri = 'mongodb://@ds239128.mlab.com:39128/ci-auth-db';
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      auth: {
+        user: DB_USER,
+        password: DB_USER_PASS
+      },
+      server: {
+        socketOptions: { keepAlive: 1 }
+      }
+    });
+  }
+
+  const connection = mongoose.connection;
+  connection.on('connected', () => {
+    console.log('Connected to db');
+  });
+
+  connection.on('disconnected', () => {
+    console.log('Disconnected to db');
+  });
+
+  connection.on('error', (error) => {
+    console.log('Db connection error ', error);
+    process.exit(1);
+  });
 }
 
 export default { initConnection };
