3b965c8340fd42d2994981dc81e7810b603f8135
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-06-22 18:55:21 -0300

    change user model

--- components/auth/user.model.js
+++ components/auth/user.model.js
@@ -1,15 +1,34 @@
 import mongoose from 'mongoose';
 
-class User extends mongoose.Schema {
-  constructor() {
-    super({
-      name: { type: String, required: [true, 'Informe o nome.'] },
-      email: { type: String, required: [true, 'Informe o email.'] },
-      password: { type: String, required: [true, 'Informe a senha.'] },
-      articles: [Number],
-      resetPasswordToken: { type: String }
-    });
-  }
-}
+// class User extends mongoose.Schema {
+//   constructor() {
+//     super({
+//       name: { type: String, required: [true, 'Informe o nome.'] },
+//       email: { type: String, required: [true, 'Informe o email.'] },
+//       password: { type: String, required: [true, 'Informe a senha.'] },
+//       articles: [Number],
+//       resetPasswordToken: { type: String }
+//     });
+//   }
+// }
 
-export default mongoose.model('User', new User());
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Informe o nome.']
+  },
+  email: {
+    type: String,
+    required: [true, 'Informe o email.']
+  },
+  password: {
+    type: String,
+    required: [true, 'Informe a senha.']
+  },
+  articles: [Number],
+  resetPasswordToken: {
+    type: String
+  }
+});
+
+export default mongoose.model('User', userSchema);
