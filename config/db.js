9c0c7389e3254ad4c18caa3e792909023322044b
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-06-25 21:25:22 -0300

    change db config

--- config/db.js
+++ config/db.js
@@ -3,7 +3,13 @@ import { NODE_ENV, DB_NAME, DB_USER, DB_USER_PASS } from 'babel-dotenv';
 
 const initConnection = async () => {
   if (NODE_ENV === 'dev') {
-    mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`, { useNewUrlParser: true });
+    mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`, {
+      useNewUrlParser: true,
+      auth: {
+        user: DB_USER,
+        password: DB_USER_PASS
+      },
+    });
   } else {
     const uri = 'mongodb://@ds239128.mlab.com:39128/ci-auth-db';
     mongoose.connect(uri, {
@@ -34,3 +40,11 @@ const initConnection = async () => {
 }
 
 export default { initConnection };
+
+// db.createUser(
+//   {
+//     user: "",
+//     pwd: "",
+//     roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+//   }
+// )
