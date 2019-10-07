125ad470e2f3bb4bc0a552d733971f5380744331
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-10-07 17:03:32 -0300

    fix babel

--- components/auth/signup.action.js
+++ components/auth/signup.action.js
@@ -11,22 +11,22 @@ const signup = async (req, res) => {
   const password = req.body.password || '';
   const confirmPassword = req.body.confirm_password || '';
 
-  if (!email.match(emailRegex)) return res.status(400).send({ errors: ['Email inválido!'] });
-  if (!password.match(passwordRegex)) return res.status(400).send({ errors: ['A senha deve conter entre 6 a 12 elementos!'] });
+  if (!email.match(emailRegex)) return res.status(400).send({ message: 'Email inválido!' });
+  if (!password.match(passwordRegex)) return res.status(400).send({ message: 'A senha deve conter entre 6 a 12 elementos!' });
 
   const salt = bcrypt.genSaltSync();
   const passwordHash = bcrypt.hashSync(password, salt);
 
-  if (!bcrypt.compareSync(confirmPassword, passwordHash)) return res.status(400).send({ errors: ['Senhas não conferem!'] });
+  if (!bcrypt.compareSync(confirmPassword, passwordHash)) return res.status(400).send({ message: 'Senhas não conferem!' });
 
   User.findOne({ email }, (err, user) => {
     if (err) return helper.sendErrorsFromDB(res, err);
-    else if (user) return res.status(400).send({ errors: ['Email já cadastrado!'] });
+    else if (user) return res.status(400).send({ message: 'Email já cadastrado!' });
 
     const newUser = new User({ name, email, password: passwordHash });
     newUser.save((err) => {
       if (err) return helper.sendErrorsFromDB(res, err);
-      return res.status(200).send({ data: true, messages: ['Registro realizado com sucesso!'] });
+      return res.status(200).send({ message: 'Registro realizado com sucesso!' });
     });
   });
 };
