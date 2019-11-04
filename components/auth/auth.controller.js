328da37cf3e2d0f42f9bc2dedfd70130e65e51a5
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-11-04 01:37:46 -0300

    fix login

--- components/auth/auth.controller.js
+++ components/auth/auth.controller.js
@@ -42,12 +42,11 @@ const login = (req, res) => {
     if (err) {
       return res.status(400).send({ message: err });
     } else if (user && bcrypt.compareSync(password, user.password)) {
-      const payload = { id: user.id, email: user.email };
+      const payload = { id: user.id, username: user.username, email: user.email };
       const options = { algorithm:  'HS256', expiresIn: '1 day' };
       const token = jwt.sign(payload, AUTH_SECRET, options);
-      const { name, email } = user;
-
-      res.json({ name, email, token });
+      const { name, username, email } = user;
+      return res.json({ name, username, email, token });
     } else {
       return res.status(400).send({ message: 'Usuário/Senha inválidos' });
     }
